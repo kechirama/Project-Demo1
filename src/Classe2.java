6d1094e563dcdaa311f9db5c07bf1cdb38a27570
Kechi <kechi.hirama@usp.br>
2017-11-16 16:03:41 -0200

    Third commit

--- src/Classe2.java
+++ src/Classe2.java
@@ -1,11 +1,14 @@
 
 public class Classe2 {
 	private int k;
-	private int l;
+	private int l = 0;
 	public void setK (int k) {
 		this.k = k;
 	}
 	public int getK() {
 		return k;
+	}
+	public void contar() {
+		l += 1;		
 	}
 }
